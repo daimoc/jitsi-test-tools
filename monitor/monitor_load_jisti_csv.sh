3139e30c5f8cf156ff2bb7001513adb9e356408f
daimoc <damien.fetis@renater.fr>
2024-09-11 09:21:25 +0200

    Add stats script for basic jitsi installation

---
+++ monitor/monitor_load_jisti_csv.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+network=$1 #"enp0s8"
+
+read rx <"/sys/class/net/$network/statistics/rx_bytes"
+read tx <"/sys/class/net/$network/statistics/tx_bytes"
+
+echo "TIME;CPU_Prosody;CPU_NGINX_MASTER;CPU_NGINX_WORKER_1;CPU_NGINX_WORKER_2;CPU_NGINX_WORKER_3;CPU_NGINX_WORKER_4;CPU_JICOFO;CPu_JVB;SOCKET_NGINX;SOCKET_LUA;rx,tx"
+
+
+while :
+do
+
+timestamp=$newtimestamp
+tsNano=`date +"%s%N"`
+newtimestamp=$(( tsNano/1000 ))
+#newtimestamp=$(date +%s.%N)
+
+read newrx <"/sys/class/net/$network/statistics/rx_bytes"
+read newtx <"/sys/class/net/$network/statistics/tx_bytes"
+
+top -b -n1 -o PID > tmp_cpu.txt
+echo -n `date +'%Y-%m-%d %H:%M:%S'`
+echo -n ";"
+grep lua tmp_cpu.txt |awk '{printf $9";"}'
+grep nginx tmp_cpu.txt|grep root |awk '{printf $9";"}'
+grep nginx tmp_cpu.txt|grep www-data |awk '{printf $9";"}'
+grep jicofo tmp_cpu.txt |awk '{printf $9";"}'
+grep jvb tmp_cpu.txt |awk '{printf $9";"}'
+echo -n `sudo netstat -putan |grep nginx |wc -l`
+echo -n ";"
+echo -n `sudo netstat -putan |grep lua |wc -l`
+echo -n ";"
+
+delta=$((newtimestamp - timestamp))
+# convert bytes to kbit/s: bytes * 8 / 1000 => bytes / 125
+echo -n "$(( ((newrx-rx) * 8 * 1000 ) / delta  ));$(( ( (newtx-tx) * 8 *1000 ) / delta ))"
+rx=$newrx
+tx=$newtx
+
+sleep 1
+echo ""
+done
